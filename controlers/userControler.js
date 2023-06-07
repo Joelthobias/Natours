1243897eebe74ab2158282d4e8aaa2e64c3ad8a5
joel <joelthobias447@gmail.com>
2023-06-07 14:53:04 +0530

    save img from postman and name in DB

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -4,30 +4,30 @@ const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory=require('./../controlers/handleFactory');
-// const multer = require('multer');
+const multer = require('multer');
 
-// const multerStorage=multer.diskStorage({
-//   destination:(req,file,cb)=>{
-//     cd(null,'/public/img/users/')
-//   },
-//   filename:(req,file,cb)=>{
-//     const ext=file.mimetype.split('/')[1];
-//     cb(null,`user-${req.user._id}-${Date.now()}.${ext}`)
-//   }
-// })
-// const multerFilter=(req,file,cb)=>{
-//   if(file.mimetype.startsWith('image')){
-//     cb(null,true)
-//   }else{
-//     cb(new AppError('It is not a image . Please Uplaod a image file !',400),false)
-//   }
-// }
+const multerStorage=multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null,'public/img/users')
+  },
+  filename:(req,file,cb)=>{
+    const ext=file.mimetype.split('/')[1];
+    cb(null,`user-${req.user._id}-${Date.now()}.${ext}`)
+  }
+})
+const multerFilter=(req,file,cb)=>{
+  if(file.mimetype.startsWith('image')){
+    cb(null,true)
+  }else{
+    cb(new AppError('It is not a image . Please Uplaod a image file !',400),false)
+  }
+}
 
-// const upload = multer({
-//   storage:multerStorage,
-//   fileFilter:multerFilter
-// })
-// exports.uploadUserPhoto=upload.single('photo');
+const upload = multer({
+  storage:multerStorage,
+  fileFilter:multerFilter
+})
+exports.uploadUserPhoto=upload.single('photo');
 
 const filterObj=(obj,...allowedFields)=>{
   const newObj={};
@@ -50,6 +50,7 @@ exports.UpdateMe=catchAsync(async (req, res, next) => {
   }
   // filter data 
   const filterBody=filterObj(req.body,'email','name')
+  if(req.file) filterBody.photo=req.file.filename
   console.log(filterBody.email);
   // Update User
   const updateUser=await User.findByIdAndUpdate(req.user._id,filterBody,{
--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
     validate:[validator.isEmail,'Provide A Valid Email']
   },
   photo: {
-    type: String
+    type: String,
+    default:'default.jpg'
   },
   role:{
     type:String,
